8b8ede90bc4e3d0dae046c584361bb0b702dbf6b
Paul DiBenedetto <pdibenedetto@users.noreply.github.com>
2019-12-28 06:18:04 -0500

    Lecture 189 completed

---
+++ src/main/java/guru/sfg/beer/inventory/service/services/BeerInventoryListener.java
@@ -0,0 +1,12 @@
+package guru.sfg.beer.inventory.service.services;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class BeerInventoryListener
+{
+}
